f6105646d52c353e1658c8f1b8abff024c42f772
samtrion <samtrion@gmail.com>
2018-05-22 16:25:53 +0200

    FIXED: Typo Mod Name ***NO_CI***

--- src/main/java/net/samtrion/compactdrawers/CompactDrawers.java
+++ src/main/java/net/samtrion/compactdrawers/CompactDrawers.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(modid = CompactDrawers.MOD_ID, name = CompactDrawers.MOD_NAME, version = CompactDrawers.MOD_VERSION, dependencies = "required-after:storagedrawers;required-after:chameleon;after:waila;", acceptedMinecraftVersions = "[1.12,1.13)")
 public class CompactDrawers {
 	public static final String MOD_ID = "compactdrawers";
-	public static final String MOD_NAME = "CompactDrawers";
+	public static final String MOD_NAME = "Compact Drawers";
 	public static final String MOD_VERSION = "@VERSION@";
 	public static final String SOURCE_PATH = "net.samtrion.compactdrawers.";
 
