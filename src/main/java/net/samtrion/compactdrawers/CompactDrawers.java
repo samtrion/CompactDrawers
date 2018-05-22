16d7cb367b528a57ccd7b8a0cbd8238998599d88
samtrion <samtrion@gmail.com>
2018-05-22 16:29:54 +0200

    Updated ModName ***NO_CI***

--- src/main/java/net/samtrion/compactdrawers/CompactDrawers.java
+++ src/main/java/net/samtrion/compactdrawers/CompactDrawers.java
@@ -2,12 +2,12 @@ package net.samtrion.compactdrawers;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = CompactDrawers.MOD_ID, name = CompactDrawers.MOD_NAME, version = CompactDrawers.MOD_VERSION, dependencies = "required-after:storagedrawers;required-after:chameleon;after:waila;", acceptedMinecraftVersions = "[1.12,1.13)")
+@Mod(modid = CompactDrawers.MOD_ID, name = CompactDrawers.MOD_NAME, version = CompactDrawers.MOD_VERSION, dependencies = CompactDrawers.MOD_DEPENDENCIES, acceptedMinecraftVersions = "[1.12,1.13)")
 public class CompactDrawers {
 	public static final String MOD_ID = "compactdrawers";
 	public static final String MOD_NAME = "Compact Drawers";
 	public static final String MOD_VERSION = "@VERSION@";
-	public static final String SOURCE_PATH = "net.samtrion.compactdrawers.";
+	public static final String MOD_DEPENDENCIES = "required-after:storagedrawers;required-after:chameleon;after:waila;";
 
 	@Mod.Instance(MOD_ID)
 	public static CompactDrawers instance;
