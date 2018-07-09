96f486a72ab4cc24700d80a140cba1cfb81bfbdd
samtrion <samtrion@gmail.com>
2018-07-09 23:37:44 +0200

    Added Config for 2By2 Compact Drawers

--- src/main/java/net/samtrion/compactdrawers/core/ModConfig.java
+++ src/main/java/net/samtrion/compactdrawers/core/ModConfig.java
@@ -11,4 +11,8 @@ public final class ModConfig {
     public static ConfigDrawer drawer2By1     = new ConfigDrawer(true, 24);
 
     public static ConfigDrawer drawer2By1Half = new ConfigDrawer(true, 12);
+
+    public static ConfigDrawer drawer2By2     = new ConfigDrawer(true, 12);
+
+    public static ConfigDrawer drawer2By2Half = new ConfigDrawer(true, 6);
 }
