7db416177a5a205917faffedcad326b4f5d5fa4b
samtrion <me@samtrion.net>
2018-12-12 11:25:07 +0100

    Removed 2by2 Drawer Code

--- src/main/java/net/samtrion/compactdrawers/core/ModConfig.java
+++ src/main/java/net/samtrion/compactdrawers/core/ModConfig.java
@@ -5,14 +5,9 @@ import net.samtrion.compactdrawers.CompactDrawers;
 
 @Config(modid = CompactDrawers.MOD_ID, category = "block")
 public final class ModConfig {
-
     public static ConfigDrawer drawerHalf     = new ConfigDrawer(true, 8);
 
     public static ConfigDrawer drawer2By1     = new ConfigDrawer(true, 24);
 
     public static ConfigDrawer drawer2By1Half = new ConfigDrawer(true, 12);
-
-    public static ConfigDrawer drawer2By2     = new ConfigDrawer(true, 12);
-
-    public static ConfigDrawer drawer2By2Half = new ConfigDrawer(true, 6);
 }
